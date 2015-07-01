cc4c580e9c8a3e6222339cadbe73d00d93745809
Shaun Smekel <shaun.smekel@digitalpacific.com.au>
2015-07-01 14:29:10 +1000

    Reverted template specification date
    Signed-off-by: Shaun Smekel <shaun.smekel@digitalpacific.com.au>

--- cloud-config/setup_etcd_cloudinit_conf.sh
+++ cloud-config/setup_etcd_cloudinit_conf.sh
@@ -5,27 +5,27 @@ ENV="/etc/environment"
 # Test for RW access to $1
 touch $ENV
 if [ $? -ne 0 ]; then
-  echo exiting, unable to modify: $ENV
-  exit 1
+    echo exiting, unable to modify: $ENV
+    exit 1
 fi
 
 # Setup environment target
 sed -i -e '/^COREOS_PUBLIC_IPV4=/d' \
     -e '/^COREOS_PRIVATE_IPV4=/d' \
     "${ENV}"
-    
+
 # We spin loop until the the IP addresses are set
 function get_ip () {
-  IF=$1
-  IP=
-  while [ 1 ]; do
-    IP=$(ifconfig $IF | awk '/inet / {print $2}')
-    if [ "$IP" != "" ]; then
-      break
-    fi
-    sleep .1
-  done
-  echo $IP
+    IF=$1
+    IP=
+    while [ 1 ]; do
+        IP=$(ifconfig $IF | awk '/inet / {print $2}')
+        if [ "$IP" != "" ]; then
+            break
+        fi
+        sleep .1
+    done
+    echo $IP
 }
 
 # Echo results of IP queries to environment file as soon as network interfaces
@@ -35,3 +35,4 @@ echo COREOS_PRIVATE_IPV4=$(get_ip eth0) >> $ENV #eno1 should be changed to your 
 A=`cat /etc/environment | grep COREOS_PRIVATE_IPV4 | cut -f2 -d "="`
 sed -i "s#=:#=${A}:#g" /run/systemd/system/etcd.service.d/20-cloudinit.conf
 systemctl daemon-reload
+
